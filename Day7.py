fe0a472097aba5878c49d47c00b64c09410804c6
Noah Cou <noahjcou@gmail.com>
2020-12-11 15:07:48 -0500

    day 7 looks rough

---
+++ Day7.py
@@ -0,0 +1,21 @@
+# Day 7
+# what are the rules?!
+
+f = open("Day7.txt", "r")
+lst = list()
+for x in f.readlines():
+    lst.append(x[:-1])
+f.close()
+
+def main1():
+    ans = 0
+
+    return ans
+
+def main2():
+    ans = 0
+
+    return ans
+
+
+print(main1(), main2())
